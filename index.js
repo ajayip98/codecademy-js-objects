166fb1f183413f0788546799b436b2b126308288
ajayip98 <ajayipnurberdiyeva98@gmail.com>
2021-07-26 21:26:36 +0300

    exercise 4-5

--- index.js
+++ index.js
@@ -29,3 +29,42 @@ const robot = {
   robot._energyLevel = 'high ';
   
   console.log(robot.recharge());
+
+  //Ex 4
+  const robot = {
+    _model: '1E78V2',
+    _energyLevel: 100,
+    get energyLevel(){
+     if(typeof this._energyLevel === 'number'){
+       return `My current energy level is ${this._energyLevel}`
+     } else {
+       return 'System malfunction: cannot retrieve energy level'
+     }
+    }
+  };
+  
+  console.log(robot.energyLevel);
+
+  
+  //Ex 5
+  const robot = {
+    _model: '1E78V2',
+    _energyLevel: 100,
+    _numOfSensors: 15,
+    get numOfSensors(){
+      if(typeof this._numOfSensors === 'number'){
+        return this._numOfSensors;
+      } else {
+        return 'Sensors are currently down.'
+      }
+    },
+    set numOfSensors(num) {
+   if(typeof num === 'number' && num >= 0) {
+     return this._numOfSensors = num
+   }else{
+     console.log('Pass in a number that is greater than or equal to 0');
+   }
+    }
+  };
+  robot.numOfSensors = 100;
+  console.log(robot.numOfSensors);
